19485f1ad8537cc3d5e736c37a255d2248f38cba
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# DXGrid - How to Apply Data Annotations
+
+
+<p>This example shows how to apply data annotation attributes to 'Product' class members.</p>
+
+<br/>
+
+
---
+++ VB/How to Apply Data Annotations/MainPage.xaml.vb
@@ -0,0 +1,22 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Net
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Animation
+Imports System.Windows.Shapes
+
+Namespace How_to_Apply_Data_Annotations
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+			grid.ItemsSource = ProductList.GetData()
+		End Sub
+	End Class
+End Namespace
